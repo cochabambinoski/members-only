eec716eefa8a36ac1ef15ef473e1376294417f95
Feber <cochabambinoski@gmail.com>
2020-01-30 09:23:57 -0400

    Fixed schema

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
 
   # Returns the hash digest of the given string.
   def self.digest(string)
-    ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Password.create(string, cost: cost)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Password.create(string, cost: cost)
   end
 
   # Returns a random token.
