5e432ae62b67bfbc03f2179370f110e627b462a9
Feber <cochabambinoski@gmail.com>
2020-01-30 21:07:44 -0400

    Fixed helpers

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -9,6 +9,26 @@ class PostController < ApplicationController
     @post = Post.new
   end
 
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user&.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
+
   def create
     @post = current_user.post.new(post_params)
     if @post.save
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
     @user = User.all
   end
 
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
   def new
     @user = User.new
   end
